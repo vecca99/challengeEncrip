ba70713f65faadfe00bd513c5de29df1154f1b53
Veronica Vazquez Alonso <131197657+vecca99@users.noreply.github.com>
2023-05-26 12:31:26 -0600

    Update javasc.js
    validando letras minúsculas y sin símbolos

--- javasc.js
+++ javasc.js
@@ -1,53 +1,57 @@
-    function encriptar(){
-    let areaTexto = document.getElementById("areaTexto").value;
-    let tituloMensaje = document.getElementById("tituloMensaje");
-    let parrafo = document.getElementById("parrafo");
-    let muneco = document.getElementById("muneco");
+  function encriptar() {
+  let areaTexto = document.getElementById("areaTexto").value;
+  let tituloMensaje = document.getElementById("tituloMensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muneco = document.getElementById("muneco");
 
-
+  const patron = /^[a-z]+$/;
+  
+  if (patron.test(areaTexto)) {
     let textoEncriptado = areaTexto
       .toLowerCase()
-      .replace(/e/gi, "enter")
-      .replace(/i/gi, "imes")
-      .replace(/a/gi, "ai")
-      .replace(/o/gi, "ober")
-      .replace(/u/gi, "ufat");
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
 
-      if (areaTexto.length != 0) {
-        document.getElementById("areaTexto").value = textoEncriptado;
-        tituloMensaje.textContent = "Texto encriptado con éxito";
-        parrafo.textContent = "";
-        muneco.src = "./encriptado.jpg";
-      } else {
-        muneco.src = "./muneco.png";
-        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        swal("Ooops!", "Debes ingresar un texto", "warning");
-      }
-    }
-    function desencriptar() {
-      const areaTexto = document.getElementById("areaTexto").value.toLowerCase();
-      const tituloMensaje = document.getElementById("tituloMensaje");
-      const parrafo = document.getElementById("parrafo");
-      const muneco = document.getElementById("muneco");
-    
-      const palabrasClave = ["enter", "imes", "ai", "ober", "ufat"];
-      const textoEncriptado = palabrasClave.reduce((texto, clave) => {
-        return texto.replace(new RegExp(clave, "gi"), clave[0]);
-      }, areaTexto);
-    
-      if (textoEncriptado !== areaTexto) {
-        document.getElementById("areaTexto").value = textoEncriptado;
-        tituloMensaje.textContent = "Texto desencriptado con éxito";
-        parrafo.textContent = "";
-        muneco.src = "./desencriptado.jpg";
-      } else {
-        muneco.src = "./muneco.png";
-        tituloMensaje.textContent = "Error al desencriptar";
-        parrafo.textContent = "Primero debes encriptar el texto para poder desencriptarlo.";
-        swal("Ooops!", "El texto no está encriptado", "warning");
-      }
-    }
+    document.getElementById("areaTexto").value = textoEncriptado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muneco.src = "./encriptado.jpg";
+  } else {
+    muneco.src = "./muneco.png";
+    tituloMensaje.textContent = "Error al encriptar";
+    parrafo.textContent = "El texto debe contener solo letras minúsculas sin acentos y sin símbolos.";
+    swal("Ooops!", "El texto no es válido", "warning");
+  }
+}
+
+function desencriptar() {
+  const areaTexto = document.getElementById("areaTexto").value.toLowerCase();
+  const tituloMensaje = document.getElementById("tituloMensaje");
+  const parrafo = document.getElementById("parrafo");
+  const muneco = document.getElementById("muneco");
+
+  const patron = /^[a-z]+$/;
+
+  if (patron.test(areaTexto)) {
+    const palabrasClave = ["enter", "imes", "ai", "ober", "ufat"];
+    const textoDesencriptado = palabrasClave.reduce((texto, clave) => {
+      return texto.replace(new RegExp(clave, "gi"), clave[0]);
+    }, areaTexto);
+
+    document.getElementById("areaTexto").value = textoDesencriptado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muneco.src = "./desencriptado.jpg";
+  } else {
+    muneco.src = "./muneco.png";
+    tituloMensaje.textContent = "Error al desencriptar";
+    parrafo.textContent = "El texto debe contener solo letras minúsculas sin acentos y sin símbolos.";
+    swal("Ooops!", "El texto no es válido", "warning");
+  }
+}
     function copiar() {
         var textarea = document.getElementById("areaTexto");
         textarea.select();
